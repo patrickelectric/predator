eb5dbb4c9f43153021960dd7967ef74eb896fb9b
Patrick <patrickelectric@gmail.com>
2014-01-18 13:03:00 -0200

    started v-2.9

---
+++ v-2.9/lib/timer.h
@@ -0,0 +1,64 @@
+#include <time.h>
+#include <ctime>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class timer
+{
+	private:
+		bool  started,ended;
+		struct timespec time_a,time_b;
+		double timeResut;
+		void clear();
+		
+	public:
+		//void init(char *Name);
+		timer() : started(false),ended(false) {};
+		void a();
+		double b();
+		double end();
+};
+
+void timer::a()
+{
+	clear();
+	clock_gettime(CLOCK_MONOTONIC ,&time_a);
+	started = true;
+}
+
+double timer::b()
+{
+	ended=true;
+	if(started==true)
+	{
+	
+	clock_gettime(CLOCK_MONOTONIC ,&time_b);
+
+	double sec  = (double)(time_b.tv_sec  - time_a.tv_sec);
+	double nano = (double)(time_b.tv_nsec - time_a.tv_nsec)*1E-9;
+	
+	timeResut = sec + nano;
+	
+	return timeResut;
+
+	}
+	else
+	{
+		printf("timer not initialized\n");
+		return -1;
+	}
+}
+
+double timer::end()
+{
+	if(ended==true)
+		return timeResut;
+	if(ended==false)
+		b();
+}
+void timer::clear()
+{
+	time_a.tv_sec=time_a.tv_nsec=time_b.tv_sec=time_b.tv_nsec=0;
+}
