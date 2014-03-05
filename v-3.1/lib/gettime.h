f0276815208683367ea97af6a7dc31e5622464fb
Douglas Wildgrube Bertol <dwbertol@gmail.com>
2014-03-05 16:19:19 -0300

    Windows port (bugs) Capação do codigo do Patrick

--- v-3.1/lib/gettime.h
+++ v-3.1/lib/gettime.h
@@ -1,38 +1,39 @@
 #include < time.h >
-#include <windows.h> //I've ommited this line.
+#include < windows.h >
+
 #if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
 #else
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
 #endif
- 
+
 struct timezone 
 {
   int  tz_minuteswest; /* minutes W of Greenwich */
   int  tz_dsttime;     /* type of dst correction */
 };
- 
+
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
   FILETIME ft;
   unsigned __int64 tmpres = 0;
-  static int tzflag;
- 
+  static int tzflag = 0;
+
   if (NULL != tv)
   {
     GetSystemTimeAsFileTime(&ft);
- 
+
     tmpres |= ft.dwHighDateTime;
     tmpres <<= 32;
     tmpres |= ft.dwLowDateTime;
- 
+
+    tmpres /= 10;  /*convert into microseconds*/
     /*converting file time to unix epoch*/
     tmpres -= DELTA_EPOCH_IN_MICROSECS; 
-    tmpres /= 10;  /*convert into microseconds*/
     tv->tv_sec = (long)(tmpres / 1000000UL);
     tv->tv_usec = (long)(tmpres % 1000000UL);
   }
- 
+
   if (NULL != tz)
   {
     if (!tzflag)
@@ -43,6 +44,6 @@ int gettimeofday(struct timeval *tv, struct timezone *tz)
     tz->tz_minuteswest = _timezone / 60;
     tz->tz_dsttime = _daylight;
   }
- 
+
   return 0;
 }
